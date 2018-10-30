a41e28677af1f69f0a63dbacfb90a99418265440
Alejandro Urdiales Carretero <alejandro.urdiales.93@gmail.com>
2018-10-31 00:01:46 +0100

    finished css test

--- test/css.test.js
+++ test/css.test.js
@@ -5,25 +5,15 @@ const rimraf = require('rimraf');
 
 
 describe('Tests for the scaffold with css', () => {
-	let step = 0;
-	beforeEach(() => {
-		switch (step) {
-			case 0:
-				return helpers.run(path.join(__dirname, '../generators/app'))
-					.withArguments(['--skip-install'])
-					.withPrompts({ folder: false, styles: 'css' })
-					.inDir(path.join(__dirname, 'tmp'));
-			default:
-				true;
-		}
-
-	});
-	afterEach(() => {
-		rimraf.sync(path.join(__dirname, 'tmp'));
-		step++;
-	});
 	it('generates a proyect with css', () => {
-		return assert.file('src/containers/HelloWorld/HelloWorld.css');
+		return helpers.run(path.join(__dirname, '../generators/app'))
+			.withArguments(['--skip-install'])
+			.withPrompts({ folder: false, styles: 'css' })
+			.inDir(path.join(__dirname, 'tmp'))
+			.then(() => {
 
+				return assert.file('src/containers/HelloWorld/HelloWorld.css');
+			});
 	});
 });
+
