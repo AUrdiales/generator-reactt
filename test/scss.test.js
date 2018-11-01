99131a464dcca86620ea0a4c4d9ba79bc658307d
Alejandro Urdiales Carretero <alejandro.urdiales.93@gmail.com>
2018-11-01 19:32:20 +0100

    implemented tests

--- test/scss.test.js
+++ test/scss.test.js
@@ -4,28 +4,29 @@ const helpers = require('yeoman-test');
 const rimraf = require('rimraf');
 
 
-describe('Tests for the scaffold with css', () => {
+describe('Tests for the scaffold with scss', () => {
 	afterEach(() => {
 		rimraf.sync(path.join(__dirname, 'tmp'));
 	});
-	it('generates a proyect with css without a folder', () => {
+	it('generates a proyect with scss without a folder', () => {
 		return helpers.run(path.join(__dirname, '../generators/app'))
 			.withArguments(['--skip-install'])
-			.withPrompts({ folder: false, styles: 'css' })
+			.withPrompts({ folder: false, styles: 'sass' })
 			.inDir(path.join(__dirname, 'tmp'))
 			.then(() => {
 
-				return assert.file('src/containers/HelloWorld/HelloWorld.css');
+				return assert.file('src/containers/HelloWorld/HelloWorld.scss');
 			});
 	});
-	it('generates a proyect with css with a folder', () => {
+
+	it('generates a proyect with scss with a folder', () => {
 		return helpers.run(path.join(__dirname, '../generators/app'))
 			.withArguments(['--skip-install'])
-			.withPrompts({ folder: true, styles: 'css' })
+			.withPrompts({ folder: true, styles: 'sass' })
 			.inDir(path.join(__dirname, 'tmp'))
 			.then(() => {
 
-				return assert.file('src/containers/HelloWorld/HelloWorld.css');
+				return assert.file('src/containers/HelloWorld/HelloWorld.scss');
 			});
 	});
 });
